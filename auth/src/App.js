406035366f5a7321d177e6b0a91db0f6d2ea632e
akhmadjon-dev <info.ahmadjon@gmail.com>
2021-06-28 22:18:13 +0500

    ...

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,22 @@
 import Form from "./Components/Form/Form";
+import { Switch, Route, Link } from "react-router-dom";
+import Users from "./Components/Users";
+import User from "./Components/User";
+import Home from "./Components/Main";
+import Header from "./Components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="form">
-      <Form />
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route path="/sign-in" component={Form} />
+        <Route path="/user-list" component={Users} />
+        <Route path="/user" component={User} />
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
